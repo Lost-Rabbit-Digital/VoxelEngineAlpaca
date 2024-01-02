efda281a60744347fcd3684f15193a918c6b334d
Boden McHale <bodenkaikarden@gmail.com>
2024-01-01 19:40:16 -0500

    Setting up meshes

---
+++ VoxelEngineAlpaca/meshes/quad_mesh.py
@@ -0,0 +1,12 @@
+from settings import *
+from meshes.base_mesh import BaseMesh
+
+class QuadMesh(BaseMesh):
+    def __init__(self, app):
+        super().__init__()
+
+        self.app = app
+        self.ctx = app.ctx
+        self.program = app.shader_program.quad
+
+    def
